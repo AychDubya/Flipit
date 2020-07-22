f6ed98ca68f72aedd6b0c13e6de4ff4701e8138c
Derek Watson <derek.watson92@gmail.com>
2020-07-22 14:43:39 -0700

    updated models/index.js for sequelize v6

--- models/index.js
+++ models/index.js
@@ -1,50 +1,37 @@
-"use strict";
+'use strict';
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(module.filename);
-// Check enivronment
-var env = process.env.NODE_ENV || "development";
-// Get config for that environment
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-// Set up sequelize based on the
+let sequelize;
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs.readdirSync(__dirname)
-  // Only pull certain files from this directory
-  .filter(function (file) {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  // Add access to this file in the db object
-  .forEach(function (file) {
-    var model = sequelize.import(path.join(__dirname, file));
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-// For each item in the db object, if it can associate, associate it with the db
-Object.keys(db).forEach(function (modelName) {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
-// Add sequelize (defined above) to the db object
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-// export the db
 module.exports = db;
