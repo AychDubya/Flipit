f8e7ca0d6cbcbef8c67a53337b1cb8fa878cef2b
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-07-28 20:28:50 -0700

    deleted console log on html controller line 293

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -290,7 +290,7 @@ router.get("/deck/:id", function (req, res) {
     }
   }).then(function (deck) {
     if (deck) {
-      console.log(req.session.user.id, deck.CreatorId)
+     
       if (deck.private === true && req.session.user.id !== deck.CreatorId) {
         res.render("error", sessionObject(req, { message: "This deck is private", link: "home"}))
       } else {
