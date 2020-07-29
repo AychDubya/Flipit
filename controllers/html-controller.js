56cbed56efada138a0425cb793a5c2c4527fa42b
Derek Watson <derek.watson92@gmail.com>
2020-07-28 20:30:57 -0700

    remove comment preventing viewing while logged out

--- controllers/html-controller.js
+++ controllers/html-controller.js
@@ -290,7 +290,6 @@ router.get("/deck/:id", function (req, res) {
     }
   }).then(function (deck) {
     if (deck) {
-      console.log(req.session.user.id, deck.CreatorId)
       if (deck.private === true && req.session.user.id !== deck.CreatorId) {
         res.render("error", sessionObject(req, { message: "This deck is private", link: "home"}))
       } else {
